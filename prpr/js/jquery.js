85c91962e85f9c1b04c9ab44eb602a35465c5314
gsw07065 <gsw07065@naver.com>
2024-09-22 20:15:42 +0900

    first commit

--- prpr/js/jquery.js
+++ prpr/js/jquery.js
@@ -1,9 +1,15 @@
 $(function(){
   $("header .nav>li").hover(function(){
-    $("header .head_bg").css({height:"250px"});
+    $("header .head_bg").css({height:"230px"});
     $("header").css({"background-color":"rgba(255, 255, 255, 1)"});
   },function(){
     $("header .head_bg").css({height:"0"});
+    $("header").css({"background-color":"rgba(255, 255, 255, .3)"});
+  });
+
+  $("header .right>li").hover(function(){
+    $("header").css({"background-color":"rgba(255, 255, 255, 1)"});
+  },function(){
     $("header").css({"background-color":"rgba(255, 255, 255, .3)"});
   });
 });
