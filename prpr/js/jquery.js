cb942dffce2c960223309e476312fefbf42d140d
gsw07065 <gsw07065@naver.com>
2024-09-23 16:25:16 +0900

    first commit

--- prpr/js/jquery.js
+++ prpr/js/jquery.js
@@ -12,4 +12,23 @@ $(function(){
   },function(){
     $("header").css({"background-color":"rgba(255, 255, 255, .3)"});
   });
+
+
+  $('#fullpage').fullpage({
+    verticalCentered: true,
+    scrollBar: true,
+    normalScrollElements: '.scrollable-element',
+    afterLoad: function(origin, destination, direction) {
+  if ($(".sec2").hasClass("active")) { 
+    $.fn.fullpage.setScrollingSpeed(0); // 속도 조정
+      $.fn.fullpage.setAllowScrolling(false);
+      $("#section2").css("overflowY","scroll");
+    
+  } else {
+      // 나머지 페이지는 다시 FullPage.js 사용
+      $.fn.fullpage.setScrollingSpeed(1000);
+      $.fn.fullpage.setAllowScrolling(true);
+    }
+  }
+});
 });
